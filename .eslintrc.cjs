d2e9d1ac1419bb6c770fbd9c5de324acaa4c9b33
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-04-21 00:50:56 +0200

    add uploaded file data to db

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,8 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-argument": "warn",
   },
 };
 
--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,17 +1,16 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import type { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
-import fs from "fs";
-import { Temporal } from "@js-temporal/polyfill";
 import type IncomingForm from "formidable/Formidable";
 import HTTPError from "~/utils/HTTPError";
+import { prisma } from "~/server/db";
+import { type Prisma } from "@prisma/client";
+import { genRandomStringServerOnly } from "~/utils/genRandomString";
 
 /**
  * Upload using multiform data, requires using name file
  */
 
-let nextUpdateUnixTime = 0;
-let currentFolderName = "temp";
 let form: IncomingForm;
 
 // disable default body parser
@@ -25,75 +24,86 @@ export default async function Upload(
   req: IncomingMessage & NextApiRequest,
   res: ServerResponse & NextApiResponse
 ) {
-  return new Promise((resolve, reject) => {
+  try {
     if (req.method !== "POST") {
-      reject(new HTTPError(405, `Method ${req.method as string} not allowed`));
-      return;
-    }
-    if (Date.now() > nextUpdateUnixTime) {
-      const now = Temporal.Now.plainDateISO();
-      const firstDayOfWeek = now.subtract({ days: now.dayOfWeek - 1 });
-      nextUpdateUnixTime = Date.now() + 3600000;
-      currentFolderName = firstDayOfWeek.toString();
-      if (!fs.existsSync("./uploads/" + currentFolderName)) {
-        fs.mkdirSync("./uploads/" + currentFolderName);
-      }
-      form = new formidable.IncomingForm({
-        multiples: true,
-        uploadDir: "./uploads/" + currentFolderName,
-        keepExtensions: true,
-        maxFileSize: 1024, // 10Gb
-        maxFieldsSize: 10 * 1024 * 1024 * 1024, // 10Gb
-        maxFiles: 1024,
-        filename: function (name, ext, _part, _form) {
-          return name + ext;
-        },
-      });
+      throw new HTTPError(405, `Method ${req.method as string} not allowed`);
     }
 
-    form.parse(req, (err: Error, fields, files) => {
-      if (err) {
-        reject(new HTTPError(500, err?.message));
-        return;
-      }
-      const { file } = files;
-      if (file == undefined) {
-        reject(new HTTPError(500, "Failed to upload file"));
-        return;
-      }
-
-      console.log(files);
-      console.log(fields);
-
-      const fileArray = Array.isArray(file) ? file : [file];
-
-      for (const file of fileArray) {
-        console.log(file.filepath, file.newFilename);
-      }
-
-      // const { originalFilename, filepath: tempFilePath } = file;
-      // const ext = (originalFilename ?? "unknown.unknown").split(".").pop();
-      // const id = originalFilename;
-      // const filePath = `./uploads/${id}.${ext}`;
-      // fs.renameSync(tempFilePath, filePath);
-
-      resolve(null);
+    form = new formidable.IncomingForm({
+      multiples: true,
+      uploadDir: "./uploads/",
+      maxFileSize: 10 * 1024 * 1024 * 1024, // 10Gb
+      maxFieldsSize: 10 * 1024 * 1024 * 1024, // 10Gb
+      maxFiles: 1024,
+      hashAlgorithm: "sha1",
     });
-  })
-    .then(() => {
-      res.status(201).json({
-        status: "success",
-        statusCode: 201,
-        message: "Success: File uploaded successfully",
+
+    const { files } = await new Promise<{
+      fields: formidable.Fields;
+      files: formidable.File | formidable.File[];
+    }>((resolve, reject) => {
+      form.parse(req, (err: Error, fields, files) => {
+        if (err) {
+          reject(new HTTPError(500, err?.message));
+          return;
+        }
+        const { file: unpackedFiles } = files;
+        if (unpackedFiles == undefined) {
+          reject(new HTTPError(500, "Failed to upload file"));
+          return;
+        }
+
+        resolve({ fields, files: unpackedFiles });
       });
-    })
-    .catch((err: HTTPError) => {
-      res.status(500).json({
+    });
+
+    const fileArray = Array.isArray(files) ? files : [files];
+
+    const newFiles: Prisma.FileCreateInput[] = fileArray.map((file) => ({
+      size: file.size,
+      filepath: file.filepath,
+      originalFilename: file.originalFilename,
+      newFilename: file.newFilename,
+      mimetype: file.mimetype,
+      hash: file.hash,
+      token: genRandomStringServerOnly(40),
+    }));
+
+    prisma.file
+      .createMany({ data: newFiles, skipDuplicates: false })
+      .then()
+      .catch((err) => {
+        throw err;
+      });
+
+    // const { originalFilename, filepath: tempFilePath } = file;
+    // const ext = (originalFilename ?? "unknown.unknown").split(".").pop();
+    // const id = originalFilename;
+    // const filePath = `./uploads/${id}.${ext}`;
+    // fs.renameSync(tempFilePath, filePath);
+
+    res.status(201).json({
+      status: "success",
+      statusCode: 201,
+      message: "Success: File uploaded successfully",
+    });
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      res.status(err.statusCode).json({
         status: "error",
         statusCode: err.statusCode,
         message: err.name + ": " + err.message,
       });
-    });
+      return;
+    } else {
+      res.status(500).json({
+        status: "error",
+        statusCode: 500,
+        message: "UnknownError",
+      });
+      return;
+    }
+  }
 }
 
 // function setMetadata(file:formidable.File[]){
