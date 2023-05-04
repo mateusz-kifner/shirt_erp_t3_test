0dc48d98a191cd7f90eca068b1e82a620302f406
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-05-05 00:46:20 +0200

    !!! REMOVED HEADLESSUI

--- src/components/editable/EditableColor.tsx
+++ src/components/editable/EditableColor.tsx
@@ -1,11 +1,16 @@
 import { useClickOutside, useClipboard, useHover } from "@mantine/hooks";
-import { useEffect, useState, CSSProperties, useMemo } from "react";
+import { useEffect, useState, type CSSProperties, useMemo, useId } from "react";
 import preventLeave from "../../utils/preventLeave";
 import { IconCopy, IconX } from "@tabler/icons-react";
-import EditableInput from "../../types/EditableInput";
+import type EditableInput from "../../types/EditableInput";
+import colorNames from "~/utils/color-names.json";
+import { RGBColor, SketchPicker } from "react-color";
+import { showNotification } from "~/lib/notifications";
+import DisplayCell from "../basic/DisplayCell";
+import { Color } from "react-color";
 
 const colorNameKeys = Object.keys(colorNames);
-const colorNamesRGB = colorNameKeys.map((val) => [
+const colorNamesRGB: [number, number, number][] = colorNameKeys.map((val) => [
   parseInt(val.substring(1, 3), 16),
   parseInt(val.substring(3, 5), 16),
   parseInt(val.substring(5, 7), 16),
@@ -67,39 +72,39 @@ interface EditableColorProps extends EditableInput<string> {
 const EditableColor = (props: EditableColorProps) => {
   const { label, value, initialValue, onSubmit, disabled, required, style } =
     props;
-  const [color, setColor] = useState<string | null>(
-    value ?? initialValue ?? null
-  );
+  const uuid = useId();
+  const [color, setColor] = useState<RGBColor | undefined>();
+  // value ?? initialValue ?? undefined
   const [focus, setFocus] = useState<boolean>(false);
   const ref = useClickOutside(() => setFocus(false));
   const clipboard = useClipboard();
   const { hovered, ref: refHover } = useHover();
 
-  const colorName = useMemo(
-    () => (color !== null ? getColorNameFromHex(color) : ""),
-    [color]
-  );
+  // const colorName = useMemo(
+  //   () => (color !== null ? getColorNameFromHex(color) : ""),
+  //   [color]
+  // );
 
-  useEffect(() => {
-    if (focus) {
-      window.addEventListener("beforeunload", preventLeave);
-    } else {
-      if (color !== value) {
-        if (!color || color === null) {
-          onSubmit?.(null);
-          setColor(null);
-          return;
-        }
-        let hex = convertTo6DigitColorCode(color);
-        if (!isNaN(parseInt(hex.substring(1), 16))) {
-          onSubmit?.(hex);
-          setColor(hex);
-        }
-      }
-      window.removeEventListener("beforeunload", preventLeave);
-    }
-    // eslint-disable-next-line
-  }, [focus]);
+  // useEffect(() => {
+  //   if (focus) {
+  //     window.addEventListener("beforeunload", preventLeave);
+  //   } else {
+  //     if (color !== value) {
+  //       if (!color || color === null) {
+  //         onSubmit?.(null);
+  //         setColor(null);
+  //         return;
+  //       }
+  //       const hex = convertTo6DigitColorCode(color);
+  //       if (!isNaN(parseInt(hex.substring(1), 16))) {
+  //         onSubmit?.(hex);
+  //         setColor(hex);
+  //       }
+  //     }
+  //     window.removeEventListener("beforeunload", preventLeave);
+  //   }
+  //   // eslint-disable-next-line
+  // }, [focus]);
 
   useEffect(() => {
     return () => {
@@ -107,13 +112,13 @@ const EditableColor = (props: EditableColorProps) => {
     };
   }, []);
 
-  useEffect(() => {
-    if (value) {
-      setColor(value);
-    } else {
-      setColor("");
-    }
-  }, [value]);
+  // useEffect(() => {
+  //   if (value) {
+  //     setColor(value);
+  //   } else {
+  //     setColor("");
+  //   }
+  // }, [value]);
 
   const onKeyDown = (e: React.KeyboardEvent<any>) => {
     if (focus) {
@@ -125,174 +130,186 @@ const EditableColor = (props: EditableColorProps) => {
   };
 
   return (
-    <Input.Wrapper
-      label={
-        label && label.length > 0 ? (
-          <>
-            {label}
-            {color && color.length > 0 && (
-              <ActionIcon
-                size="xs"
-                style={{
-                  display: "inline-block",
-                  transform: "translate(4px, 4px)",
-                }}
+    <div
+      className="flex-grow"
+      onClick={() => !disabled && setFocus(true)}
+      onFocus={() => !disabled && setFocus(true)}
+    >
+      {label && (
+        <label
+          htmlFor={"textarea_" + uuid}
+          className="
+          text-sm
+          dark:text-stone-300"
+        >
+          <div className="flex items-center py-1">
+            {label}{" "}
+            {/* {color && color.length > 0 && (
+              <button
+                className="border-1 inline-flex animate-pop items-center justify-center
+            gap-3 rounded-md  stroke-gray-200 p-1 font-semibold uppercase
+          text-gray-200 no-underline transition-all  
+          hover:bg-black hover:bg-opacity-30
+            active:hover:scale-95 active:hover:animate-none 
+            active:focus:scale-95 active:focus:animate-none"
                 onClick={() => {
                   clipboard.copy(color);
                   showNotification({
                     title: "Skopiowano do schowka",
                     message: color,
+                    icon: <IconCopy />,
                   });
                 }}
                 tabIndex={-1}
               >
                 <IconCopy size={16} />
-              </ActionIcon>
-            )}
-          </>
-        ) : undefined
-      }
-      labelElement="div"
-      required={required}
-      style={style}
-      ref={refHover}
-      onClick={() => setFocus(true)}
-      onFocus={() => setFocus(true)}
-    >
+              </button>
+            )} */}
+          </div>
+        </label>
+      )}
       <div ref={ref} style={{ position: "relative" }}>
         {focus ? (
-          <ColorInput
-            swatchesPerRow={7}
-            format="hex"
-            swatches={[
-              "#000e1c",
-              "#ce102c",
-              "#002a3a",
-              "#194E90",
-              "#7E7B50",
-              "#007398",
-              "#AC9768",
-              "#3B8DDF",
-              "#DB5204",
-              "#294634",
-              "#845D32",
-              "#512B3A",
-              "#5D3225",
-              "#A8353A",
-              "#343E48",
-              "#B42574",
-              "#303030",
-              "#D0E1D9",
-              "#018657",
-              "#4F4A34",
-              "#77BC21",
-              "#D5BA8D",
-              "#C3A0D8",
-              "#F0E87D",
-              "#FF681F",
-              "#FFA401",
-              "#F8DBE0",
-              "#ECED6E",
-              "#2F1A45",
-              "#AF0061",
-              "#E2B87E",
-              "#97D5EA",
-              "#FFCD0E",
-              "#FF8041",
-              "#00A6B6",
-              "#F52837",
-              "#888A8E",
-              "#F54D80",
-              "#42201F",
-              "#F6DC6D",
-              "#FFBFA1",
-              "#FFB1C1",
-              "#BADCE6",
-              "#FFB81E",
-              "#96E3C1",
-              "#c19473",
-              "#426D8C",
-              "#555555",
-              "#ffffff",
-              "#25262b",
-              "#868e96",
-              "#fa5252",
-              "#e64980",
-              "#be4bdb",
-              "#7950f2",
-              "#4c6ef5",
-              "#228be6",
-              "#15aabf",
-              "#12b886",
-              "#40c057",
-              "#82c91e",
-              "#fab005",
-              "#fd7e14",
-            ]}
-            value={color ?? "#ffffff"}
-            onChange={(new_hex) => {
-              setColor(new_hex);
+          <SketchPicker
+            color={color}
+            onChange={(color) => {
+              setColor(color.rgb);
             }}
-            disabled={disabled}
-            required={required}
-            styles={{ input: { minHeight: 44 } }}
-            withinPortal={false}
-            onKeyDown={onKeyDown}
-            withPicker={true}
+            styles={{ default: {} }}
           />
         ) : (
-          <Text
-            sx={[
-              SxBorder,
-              (theme) => ({
-                width: "100%",
+          // <ColorInput
+          //   swatchesPerRow={7}
+          //   format="hex"
+          //   swatches={[
+          //     "#000e1c",
+          //     "#ce102c",
+          //     "#002a3a",
+          //     "#194E90",
+          //     "#7E7B50",
+          //     "#007398",
+          //     "#AC9768",
+          //     "#3B8DDF",
+          //     "#DB5204",
+          //     "#294634",
+          //     "#845D32",
+          //     "#512B3A",
+          //     "#5D3225",
+          //     "#A8353A",
+          //     "#343E48",
+          //     "#B42574",
+          //     "#303030",
+          //     "#D0E1D9",
+          //     "#018657",
+          //     "#4F4A34",
+          //     "#77BC21",
+          //     "#D5BA8D",
+          //     "#C3A0D8",
+          //     "#F0E87D",
+          //     "#FF681F",
+          //     "#FFA401",
+          //     "#F8DBE0",
+          //     "#ECED6E",
+          //     "#2F1A45",
+          //     "#AF0061",
+          //     "#E2B87E",
+          //     "#97D5EA",
+          //     "#FFCD0E",
+          //     "#FF8041",
+          //     "#00A6B6",
+          //     "#F52837",
+          //     "#888A8E",
+          //     "#F54D80",
+          //     "#42201F",
+          //     "#F6DC6D",
+          //     "#FFBFA1",
+          //     "#FFB1C1",
+          //     "#BADCE6",
+          //     "#FFB81E",
+          //     "#96E3C1",
+          //     "#c19473",
+          //     "#426D8C",
+          //     "#555555",
+          //     "#ffffff",
+          //     "#25262b",
+          //     "#868e96",
+          //     "#fa5252",
+          //     "#e64980",
+          //     "#be4bdb",
+          //     "#7950f2",
+          //     "#4c6ef5",
+          //     "#228be6",
+          //     "#15aabf",
+          //     "#12b886",
+          //     "#40c057",
+          //     "#82c91e",
+          //     "#fab005",
+          //     "#fd7e14",
+          //   ]}
+          //   value={color ?? "#ffffff"}
+          //   onChange={(new_hex) => {
+          //     setColor(new_hex);
+          //   }}
+          //   disabled={disabled}
+          //   required={required}
+          //   styles={{ input: { minHeight: 44 } }}
+          //   withinPortal={false}
+          //   onKeyDown={onKeyDown}
+          //   withPicker={true}
+          // />
+          // <Text
+          //   sx={[
+          //     SxBorder,
+          //     (theme) => ({
+          //       width: "100%",
 
-                fontSize: theme.fontSizes.sm,
-                wordBreak: "break-word",
-                whiteSpace: "pre-line",
-                padding: "10px 16px",
-                paddingRight: 32,
-                minHeight: 44,
-                lineHeight: 1.55,
-                paddingLeft: 36,
-                "&:before": {
-                  content: "''",
-                  position: "absolute",
-                  height: 24,
-                  width: 24,
-                  top: 9,
-                  left: 6,
-                  backgroundColor: color ?? undefined,
-                  borderRadius: "100%",
-                  border:
-                    theme.colorScheme === "dark"
-                      ? "1px solid #2C2E33"
-                      : "1px solid #ced4da",
-                },
-                border:
-                  focus || hovered
-                    ? theme.colorScheme === "dark"
-                      ? "1px solid #2C2E33"
-                      : "1px solid #ced4da"
-                    : "1px solid transparent",
-                borderColor: colorName === "Nieznany" ? "#f00" : undefined,
+          //       fontSize: theme.fontSizes.sm,
+          //       wordBreak: "break-word",
+          //       whiteSpace: "pre-line",
+          //       padding: "10px 16px",
+          //       paddingRight: 32,
+          //       minHeight: 44,
+          //       lineHeight: 1.55,
+          //       paddingLeft: 36,
+          //       "&:before": {
+          //         content: "''",
+          //         position: "absolute",
+          //         height: 24,
+          //         width: 24,
+          //         top: 9,
+          //         left: 6,
+          //         backgroundColor: color ?? undefined,
+          //         borderRadius: "100%",
+          //         border:
+          //           theme.colorScheme === "dark"
+          //             ? "1px solid #2C2E33"
+          //             : "1px solid #ced4da",
+          //       },
+          //       border:
+          //         focus || hovered
+          //           ? theme.colorScheme === "dark"
+          //             ? "1px solid #2C2E33"
+          //             : "1px solid #ced4da"
+          //           : "1px solid transparent",
+          //       borderColor: colorName === "Nieznany" ? "#f00" : undefined,
 
-                "&:focus": {
-                  borderColor: focus
-                    ? undefined
-                    : theme.colorScheme === "dark"
-                    ? theme.colors.dark[5]
-                    : theme.colors.gray[4],
-                },
-              }),
-              SxRadius,
-            ]}
-          >
-            {colorName || "⸺"}
-          </Text>
+          //       "&:focus": {
+          //         borderColor: focus
+          //           ? undefined
+          //           : theme.colorScheme === "dark"
+          //           ? theme.colors.dark[5]
+          //           : theme.colors.gray[4],
+          //       },
+          //     }),
+          //     SxRadius,
+          //   ]}
+          // >
+          //   {colorName || "⸺"}
+          // </Text>
+          <DisplayCell>test</DisplayCell>
         )}
       </div>
-    </Input.Wrapper>
+    </div>
   );
 };
 
