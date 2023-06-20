d792ee4db6d1c003e07d902dc564203b63f632f5
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-06-21 01:05:10 +0200

    files working

--- src/components/editable/EditableFiles.tsx
+++ src/components/editable/EditableFiles.tsx
@@ -6,17 +6,12 @@ import {
   type DragEvent,
 } from "react";
 
-import { useClickOutside, useHover } from "@mantine/hooks";
 import {
   IconArrowDown,
   IconArrowUp,
-  IconPhoto,
   IconPlus,
   IconTrashX,
-  IconUpload,
-  IconX,
 } from "@tabler/icons-react";
-// import FileListItem from "../FileListItem";
 
 import * as RadixContextMenu from "@radix-ui/react-context-menu";
 import { IconLoader2 } from "@tabler/icons-react";
@@ -24,38 +19,11 @@ import useTranslation from "~/hooks/useTranslation";
 import useUploadMutation from "~/hooks/useUploadMutation";
 import { type FileType } from "~/schema/fileSchema";
 import type EditableInput from "~/types/EditableInput";
-import type TablerIconType from "~/types/TablerIconType";
 import FileListItem from "../FileListItem";
 import Modal from "../basic/Modal";
 import InputLabel from "../input/InputLabel";
 
 // FIXME: ENFORCE FILE LIMIT
-
-// function getIconColor(status: any) {
-//   return status.accepted
-//     ? //@ts-ignore
-//       theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6]
-//     : status.rejected
-//     ? theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]
-//     : theme.colorScheme === "dark"
-//     ? theme.colors.dark[0]
-//     : theme.colors.gray[7];
-// }
-
-function ImageUploadIcon({
-  status,
-  ...props
-}: React.ComponentProps<TablerIconType> & { status: any }) {
-  if (status.accepted) {
-    return <IconUpload {...props} />;
-  }
-
-  if (status.rejected) {
-    return <IconX {...props} />;
-  }
-
-  return <IconPhoto {...props} />;
-}
 
 interface EditableFilesProps extends EditableInput<FileType[]> {
   maxCount?: number;
@@ -73,7 +41,6 @@ const EditableFiles = (props: EditableFilesProps) => {
   } = props;
   const t = useTranslation();
   const uuid = useId();
-  const [focus, setFocus] = useState<boolean>(false);
   const [files, setFiles] = useState<FileType[]>(value ?? initialValue ?? []);
   const [error, setError] = useState<string | undefined>();
   const [uploading, setUploading] = useState<number>(0);
@@ -84,8 +51,6 @@ const EditableFiles = (props: EditableFilesProps) => {
     height: number;
   }>({ url: "", width: 100, height: 100 });
   const [dragActive, setDragActive] = useState<boolean>(false);
-  const refClickOutside = useClickOutside(() => setFocus(false));
-  const { hovered, ref: hoverdRef } = useHover();
   const { mutate: uploadMutate } = useUploadMutation({
     onSuccess: (data: Response, variables: FormData, context: unknown) => {
       data
@@ -123,20 +88,6 @@ const EditableFiles = (props: EditableFilesProps) => {
     }
 
     uploadMutate(formData);
-
-    // axios
-    //   .post(env.NEXT_PUBLIC_SERVER_API_URL + "/api/upload", formData)
-    //   .then((res: any) => {
-    //     const filesData = res.data;
-    //     onSubmit?.([...files, ...filesData]);
-    //     setFiles((files) => [...files, ...filesData]);
-    //     setUploading((num: number) => num - new_files.length);
-    //     setError(undefined);
-    //   })
-    //   .catch((err: AxiosError) => {
-    //     setError(err.response?.statusText);
-    //     setUploading((num: number) => num - new_files.length);
-    //   });
   };
 
   const onDelete = (index: number) => {
@@ -174,66 +125,19 @@ const EditableFiles = (props: EditableFilesProps) => {
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    try {
-      if (e.target.files && e.target.files[0]) {
-        console.log("files handleChange", e.target.files[0]);
-        // at least one file has been selected
-
-        // validate file type
-        // const valid = validateFileType(e.target.files[0])
-        // if (!valid) {
-        //   toast({
-        //     title: 'Invalid file type',
-        //     description: 'Please upload a valid file type.',
-        //   })
-        //   return
-        // }
-
-        // const { getUrl, error } = await s3Upload(e.target.files[0])
-        // if (!getUrl || error) throw new Error('Error uploading file')
-
-        const { name, size } = e.target.files[0];
-
-        // addFilesToState([{ name, getUrl, size }])
-      }
-    } catch (error) {
-      // already handled
+    if (e.target.files && e.target.files.length > 0) {
+      onUploadMany(Array.from(e.target.files));
+      console.log("files handleChange", Array.from(e.target.files));
     }
   };
 
   const handleDrop = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-
-    // validate file type
-    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-      const files = Array.from(e.dataTransfer.files);
-      onUploadMany(files);
-      console.log("files handleDrop", files);
-      // const validFiles = files.filter((file) => validateFileType(file))
-
-      // if (files.length !== validFiles.length) {
-      //   toast({
-      //     title: 'Invalid file type',
-      //     description: 'Only image files are allowed.',
-      //   })
-      // }
-
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      onUploadMany(Array.from(e.dataTransfer.files));
       try {
-        // const filesWithUrl = await Promise.all(
-        //   validFiles.map(async (file) => {
-        //     const { name, size } = file
-        //     const { getUrl, error } = await s3Upload(file)
-
-        //     if (!getUrl || error) return { name, size, getUrl: '', error }
-        //     return { name, size, getUrl }
-        //   })
-        // )
-
         setDragActive(false);
-
-        // at least one file has been selected
-        // addFilesToState(filesWithUrl)
 
         e.dataTransfer.clearData();
       } catch (error) {
@@ -244,9 +148,9 @@ const EditableFiles = (props: EditableFilesProps) => {
 
   return (
     <div
-      onClick={() => !disabled && setFocus(true)}
-      onFocus={() => !disabled && setFocus(true)}
-      ref={refClickOutside}
+      // onClick={() => !disabled && setFocus(true)}
+      // onFocus={() => !disabled && setFocus(true)}
+      // ref={refClickOutside}
       onDragEnter={handleDrag}
     >
       <InputLabel
@@ -268,7 +172,6 @@ const EditableFiles = (props: EditableFilesProps) => {
         </Modal>
 
         <div
-          ref={hoverdRef}
           className={`relative min-h-[44px]  rounded border border-solid transition-all ${
             dragActive ? "border-sky-600" : "border-transparent"
           }`}
@@ -316,7 +219,6 @@ const EditableFiles = (props: EditableFilesProps) => {
               ))
             : !uploading && <div>⸺</div>}
           <div className="relative w-full">
-            {/* <Button className="absolute inset-0"> test </Button> */}
             <label
               htmlFor="file"
               className="inline-flex
@@ -356,50 +258,11 @@ const EditableFiles = (props: EditableFilesProps) => {
               id="file"
               type="file"
               className="absolute inset-0 -z-10"
-              // className={`${uploading ? "" : "hidden"}`}
-              // className={`relative w-full cursor-pointer after:absolute after:inset-0 after:w-full after:bg-red-500 ${
-              //   uploading
-              //     ? "after:content-['uploading']"
-              //     : "after:content-['choose file']"
-              // }`}
+              onChange={handleChange}
+              multiple
             />
           </div>
           {error && <div className="text-red-500">{error}</div>}
-          {/* {(active && focus) ||
-            (!!uploading && (
-              //   ? (files.length < maxFileCount || !!uploading) &&
-              //     !disabled && (
-              <Button
-                variant="default"
-                styles={(theme) => ({
-                  root: {
-                    height: 46,
-                    width: "100%",
-                    backgroundColor:
-                      theme.colorScheme === "dark" ? "#1A1B1E" : "#fff",
-                    "&:disabled": {
-                      color: theme.colorScheme === "dark" ? "#fff" : "#000",
-                    },
-                  },
-                })}
-                onClick={() => !uploading && setDropOpened(true)}
-                radius="sm"
-              >
-                {uploading ? (
-                  <Group align="center" position="center">
-                    <Loader />
-                    <Text>
-                      {t("uploading")} {uploading}{" "}
-                      {uploading === 1
-                        ? t("files.singular")
-                        : t("files.plural")}
-                    </Text>
-                  </Group>
-                ) : (
-                  <Plus size={26} />
-                )}
-              </Button>
-            ))} */}
         </div>
       </div>
     </div>
